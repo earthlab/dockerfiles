eb5205fe58b15d5ff9f560ce43e36cc5f98237de
jvtcl <joseph.tuccillo@colorado.edu>
2019-05-30 16:09:22 -0600

    update activate environment

--- r-python-eds-lessons/import_check.py
+++ r-python-eds-lessons/import_check.py
@@ -1,3 +1,15 @@
+# libraries with many dependencies
 import earthpy
 import rasterio
 import folium
+
+# pip installaed libraries
+import nbclean
+import mne
+import kiwisolver
+import mapboxgl
+import hydrofunctions
+import cenpy
+import climata
+import elevation
+import richdem
